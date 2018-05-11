7f0011db148330f078fe80b0e35c6eae4364c59a
ziaulTest <38961758+ziaulTest@users.noreply.github.com>
2018-05-11 10:14:21 +0100

    Add files via upload

---
+++ GitTest/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type your name?\n");
+            string s = Console.ReadLine();
+            Console.WriteLine(s+", hello :)");
+            
+        
+            Console.ReadKey();
+        }
+    }
+}
